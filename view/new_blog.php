0be41c818141f23061a5626b918fea8c4e10b89d
KikiOduro <akua.oduro@ashesi.edu.gh>
2024-12-17 16:02:07 +0000

    new

--- view/new_blog.php
+++ view/new_blog.php
@@ -287,6 +287,7 @@
 <body>
     <header class="header">
         <h1>VogueVerse</h1>
+        <a href="../index.php" style="padding-left: 90px;">Go back to homepage</a>
     </header>
 
     <div class="main-container">
